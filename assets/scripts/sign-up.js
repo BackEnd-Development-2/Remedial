c5a502f2e26f3170010af37dd8c166798f76a8f7
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-05-03 23:41:34 +0700

    add logic for sign up

--- assets/scripts/sign-up.js
+++ assets/scripts/sign-up.js
@@ -14,3 +14,36 @@ eyePasCon.addEventListener("click", function(){
     const type = conPasswordInput.getAttribute("type") === "password" ? "text" : "password"
     conPasswordInput.setAttribute("type", type)
 })
+
+const addDataUser = async (data) => {
+    try {
+      const response = await fetch("https://645209d8bce0b0a0f73af0c9.mockapi.io/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+    
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+}
+
+const nama = document.getElementById('nama')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+
+
+document.getElementById('submit').addEventListener('click', () => {
+    const data = {
+        nama : nama.value,
+        email : email.value,
+        password : password.value
+    }
+    
+    addDataUser(data)
+})
+
