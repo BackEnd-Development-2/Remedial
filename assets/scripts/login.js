d9783da90a1d1116086b9fbd41d35fc997d003ea
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-05-03 17:25:40 +0700

    add logic for login

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -6,3 +6,37 @@ eye.addEventListener("click", function(){
     const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
     passwordInput.setAttribute("type", type)
 })
+
+const getDataUser = async () => {
+    const response = await fetch('https://645209d8bce0b0a0f73af0c9.mockapi.io/users')
+    if (!response.ok) {
+        throw new Error('API request failed')
+    }
+    const data = await response.json()
+    return data
+}
+
+const inputUser = {
+    email : document.getElementById('email'),
+    password : document.getElementById('password')
+}
+
+const checkLogin = async () => {
+    const dataUsers = await getDataUser()
+    
+    for (item of dataUsers) {
+        if (item.email === inputUser.email.value && item.password === inputUser.password.value){
+          window.location.href = 'index.html'
+          break
+        } else {
+          const warning = document.getElementById('warning')
+          warning.style.display = 'block'
+          warning.innerText = 'username atau password salah'
+          break
+        }
+      }
+}
+
+document.getElementById('submit').addEventListener('click', async () => {   
+    checkLogin()
+})
