9c1cea55da59f935be5511430b702a9a60055d53
Yosua Mual Pardomuan Hutasoit <yosuahutasoit082@gmail.com>
2023-05-04 18:44:39 +0700

    update kelas and create js

--- Kelas.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', function() {
-  // Set cookies
-  document.cookie = "login_required=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
-});
