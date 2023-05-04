ffa9deceaef56015215553042d2f6f7b3c2f7f44
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-05-04 20:47:01 +0700

    update show/hide input password

--- assets/scripts/eye-password.js
+++ assets/scripts/eye-password.js
@@ -1,10 +1,18 @@
 const passwordInput = document.querySelector("#password")
-const eye = document.querySelector("#eye")
+const eyePas = document.querySelector("#eye-pas")
+const conPasswordInput = document.querySelector("#co-password")
+const eyePasCon = document.querySelector("#eye-pasCon")
 
-const eyePassword = eye.addEventListener("click", function(){
+const eyePassword = eyePas.addEventListener("click", function(){
     this.classList.toggle("fa-eye-slash")
     const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
     passwordInput.setAttribute("type", type)
 })
 
-export { eyePassword }
+const eyeConfPassword = eyePasCon.addEventListener("click", function(){
+    this.classList.toggle("fa-eye-slash")
+    const type = conPasswordInput.getAttribute("type") === "password" ? "text" : "password"
+    conPasswordInput.setAttribute("type", type)
+})
+
+export { eyePassword, eyeConfPassword }
--- assets/scripts/sign-up.js
+++ assets/scripts/sign-up.js
@@ -1,19 +1,4 @@
-const passwordInput = document.querySelector("#password")
-const conPasswordInput = document.querySelector("#co-password")
-const eyePas = document.querySelector("#eye-pas")
-const eyePasCon = document.querySelector("#eye-pasCon")
-
-eyePas.addEventListener("click", function(){
-    this.classList.toggle("fa-eye-slash")
-    const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
-    passwordInput.setAttribute("type", type)
-})
-
-eyePasCon.addEventListener("click", function(){
-    this.classList.toggle("fa-eye-slash")
-    const type = conPasswordInput.getAttribute("type") === "password" ? "text" : "password"
-    conPasswordInput.setAttribute("type", type)
-})
+import { eyePassword, eyeConfPassword } from "./eye-password.js";
 
 const addDataUser = async (data) => {
     try {
