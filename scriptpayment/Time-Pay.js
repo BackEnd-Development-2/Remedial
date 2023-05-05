bf4ffcb7575e638148723d5be45d13fe9bb092e1
Yosua Mual Pardomuan Hutasoit <yosuahutasoit082@gmail.com>
2023-05-05 19:52:29 +0700

    update payment and create js

---
+++ scriptpayment/Time-Pay.js
@@ -0,0 +1,26 @@
+// Set waktu akhir menjadi 1 menit dari sekarang
+var countDownDate = new Date();
+countDownDate.setMinutes(countDownDate.getMinutes() + 2);
+
+// Update waktu setiap 1 detik
+var x = setInterval(function() {
+
+  // Ambil waktu sekarang
+  var now = new Date().getTime();
+
+  // Hitung selisih waktu antara waktu akhir dan waktu sekarang
+  var distance = countDownDate - now;
+
+  // Hitung waktu dalam jam, menit, dan detik
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Tampilkan waktu dalam format "menit:detik"
+  document.getElementById("Time-Pay").innerHTML = "Selesaikan Pembayaran dalam " + minutes + ":" + seconds;
+
+  // Jika waktu sudah berakhir, tampilkan pesan
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("Time-Pay").innerHTML = "Waktu telah berakhir";
+  }
+}, 1000);
