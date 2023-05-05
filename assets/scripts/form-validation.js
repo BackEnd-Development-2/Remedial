4b7eb08ad2457b516b89e10e9ca583c5018615bd
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-05-05 13:55:04 +0700

    update form validation

--- assets/scripts/form-validation.js
+++ assets/scripts/form-validation.js
@@ -2,14 +2,11 @@ const email = document.getElementById('email')
 const emailError = document.querySelector('#email + span.error')
 const password = document.getElementById('password')
 const passwordError = document.querySelector('#password + span.error')
-const btnSubmit = document.getElementById('submit')
-
-btnSubmit.disabled = true
 
 let emailValid = false
 let passwordValid = false
 
-const emailValidation = email.addEventListener('input', () => {
+const emailValidation = () => {
     if (email.validity.valid) {
         emailError.textContent = ''
         email.className = ''
@@ -17,50 +14,44 @@ const emailValidation = email.addEventListener('input', () => {
         emailValid = true
     } else {
         showEmailError()
+        emailValid = false
     }
-    btnDisabled()
-})
+}
 
 const showEmailError = () => {
     if (email.validity.valueMissing) {
-        emailError.textContent = "You need to enter an email address.";
+        emailError.textContent = "Masukkan alamat email kamu.";
     } else if (email.validity.typeMismatch) {
-        emailError.textContent = "Entered value needs to be an email address.";
+        emailError.textContent = "Masukkan alamat email yang valid.";
     } else if (email.validity.tooShort) {
-        emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+        emailError.textContent = `Email minimal ${email.minLength} karakter.`;
     }
 
     email.className = "input invalid"
     emailError.className = 'error active'
 }
 
-const passwordValidation = password.addEventListener('input', () => {
+const passwordValidation = () => {
     if (password.validity.valid) {
         passwordError.textContent = ''
         password.className = ''
         passwordError.className = 'error'
         passwordValid = true
     } else {
-        showPasswordError();
+        showPasswordError()
+        passwordValid = false
     }
-    btnDisabled()
-})
+}
 
 const showPasswordError = () => {
     if (password.validity.valueMissing) {
-        passwordError.textContent = "You need to enter password.";
+        passwordError.textContent = "Masukkan password kamu.";
     } else if (password.validity.tooShort) {
-        passwordError.textContent = `Password should be at least ${password.minLength} characters; you entered ${password.value.length}.`;
+        passwordError.textContent = `Password minimal ${password.minLength} karakter.`;
     }
 
     password.className = "input invalid"
     passwordError.className = 'error active'
 }
 
-const btnDisabled = () => {
-    if (emailValid === true && passwordValid === true){
-        btnSubmit.disabled = false
-    }
-}
-
-export { emailValidation, passwordValidation}
+export { emailValidation, passwordValidation, emailValid, passwordValid}
