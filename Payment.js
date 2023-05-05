d7f3a473989373eef9332e5510c0727ea5aa8505
Yosua Mual Pardomuan Hutasoit <yosuahutasoit082@gmail.com>
2023-05-05 16:44:53 +0700

    update Payment

---
+++ Payment.js
@@ -0,0 +1,102 @@
+[
+    {
+     "FullName": "Udin",
+     "JenisKelamin": "Laki-Laki",
+     "Email": "udintheboss@gmail.com",
+     "AsalSekolah": "Universitas PastiBisa",
+     "AlamatRumah": "Jl.Camar",
+     "Kota": "Bogor",
+     "Alasan": "Karena Ingin Menambah Softskill dan Hardskill",
+     "id": "1"
+    },
+    {
+     "FullName": "FullName 2",
+     "JenisKelamin": "JenisKelamin 2",
+     "Email": "Email 2",
+     "AsalSekolah": "AsalSekolah 2",
+     "AlamatRumah": "AlamatRumah 2",
+     "Kota": "Kota 2",
+     "Alasan": "Alasan 2",
+     "id": "2"
+    },
+    {
+     "FullName": "FullName 3",
+     "JenisKelamin": "JenisKelamin 3",
+     "Email": "Email 3",
+     "AsalSekolah": "AsalSekolah 3",
+     "AlamatRumah": "AlamatRumah 3",
+     "Kota": "Kota 3",
+     "Alasan": "Alasan 3",
+     "id": "3"
+    },
+    {
+     "FullName": "FullName 4",
+     "JenisKelamin": "JenisKelamin 4",
+     "Email": "Email 4",
+     "AsalSekolah": "AsalSekolah 4",
+     "AlamatRumah": "AlamatRumah 4",
+     "Kota": "Kota 4",
+     "Alasan": "Alasan 4",
+     "id": "4"
+    },
+    {
+     "FullName": "FullName 5",
+     "JenisKelamin": "JenisKelamin 5",
+     "Email": "Email 5",
+     "AsalSekolah": "AsalSekolah 5",
+     "AlamatRumah": "AlamatRumah 5",
+     "Kota": "Kota 5",
+     "Alasan": "Alasan 5",
+     "id": "5"
+    },
+    {
+     "FullName": "FullName 6",
+     "JenisKelamin": "JenisKelamin 6",
+     "Email": "Email 6",
+     "AsalSekolah": "AsalSekolah 6",
+     "AlamatRumah": "AlamatRumah 6",
+     "Kota": "Kota 6",
+     "Alasan": "Alasan 6",
+     "id": "6"
+    },
+    {
+     "FullName": "FullName 7",
+     "JenisKelamin": "JenisKelamin 7",
+     "Email": "Email 7",
+     "AsalSekolah": "AsalSekolah 7",
+     "AlamatRumah": "AlamatRumah 7",
+     "Kota": "Kota 7",
+     "Alasan": "Alasan 7",
+     "id": "7"
+    },
+    {
+     "FullName": "FullName 8",
+     "JenisKelamin": "JenisKelamin 8",
+     "Email": "Email 8",
+     "AsalSekolah": "AsalSekolah 8",
+     "AlamatRumah": "AlamatRumah 8",
+     "Kota": "Kota 8",
+     "Alasan": "Alasan 8",
+     "id": "8"
+    },
+    {
+     "FullName": "FullName 9",
+     "JenisKelamin": "JenisKelamin 9",
+     "Email": "Email 9",
+     "AsalSekolah": "AsalSekolah 9",
+     "AlamatRumah": "AlamatRumah 9",
+     "Kota": "Kota 9",
+     "Alasan": "Alasan 9",
+     "id": "9"
+    },
+    {
+     "FullName": "FullName 10",
+     "JenisKelamin": "JenisKelamin 10",
+     "Email": "Email 10",
+     "AsalSekolah": "AsalSekolah 10",
+     "AlamatRumah": "AlamatRumah 10",
+     "Kota": "Kota 10",
+     "Alasan": "Alasan 10",
+     "id": "10"
+    }
+   ]
