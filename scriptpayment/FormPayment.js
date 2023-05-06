a407e01b45b8a6cdad1b6dbb775bd72da632ccea
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-05-06 17:27:13 +0700

    update payment

--- scriptpayment/FormPayment.js
+++ scriptpayment/FormPayment.js
@@ -1,21 +1,45 @@
-const form = document.querySelector('form');
+const btnSubmit = document.querySelector('#Payment');
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-
-  const formData = new FormData(form);
-  fetch('https://64549e1cf803f345762cc566.mockapi.io/Users', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => {
-    if (response.ok) {
-      alert('Form submitted successfully');
-      form.reset();
+const addDataUser = async (data) => {
+  try {
+      const response = await fetch("https://64549e1cf803f345762cc566.mockapi.io/Users", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+      });
+  
+      const result = await response.json();
       window.location.href = "DetailPayment.html";
-    } else {
-      throw new Error('Form submission failed');
+      console.log('a')
+  } catch (error) {
+      warning.innerText = `Error: ${error}`
+  }
+}
+
+btnSubmit.addEventListener('click', (event) => {
+event.preventDefault()
+  const gender = document.querySelectorAll('input[name="gender"]');
+  let genderUser
+
+  gender.forEach((gender) => {
+    if (gender.checked) {
+      genderUser = gender.value;
     }
-  })
-  .catch(error => console.error(error));
+  });
+
+  const dataUser = {
+    Name : document.getElementById('full-name').value,
+    JenisKelamin : genderUser,
+    Nomor : document.getElementById('phone').value,
+    Email : document.getElementById('email').value,
+    AsalInstansi : document.getElementById('asal-sekolah').value,
+    Alamat : document.getElementById('address').value,
+    Kota : document.getElementById('city').value,
+    Alasan : document.getElementById('state').value
+  }
+
+  addDataUser(dataUser)
+
 });
--- scriptpayment/script.js
+++ scriptpayment/script.js
@@ -1,6 +1,7 @@
 const couponInput = document.querySelector('#Coupon-Discount');
 const applyButton = document.querySelector('#Submit-Discount');
 const priceLabel = document.querySelector('#Price'); // memilih label harga dengan id "Price"
+let harga = 550000
 
 applyButton.addEventListener('click', () => {
   const couponCode = couponInput.value;
@@ -8,8 +9,8 @@ applyButton.addEventListener('click', () => {
     alert('Please enter a coupon code.');
   } else if (couponCode === 'BE-2') {
     alert(`Coupon code ${couponCode} has been applied. You got discount.`);
-    const discountedPrice = 200000; // menghitung harga setelah didiskon
-    priceLabel.textContent = `Rp. ${discountedPrice.toLocaleString()}`; // memperbarui nilai label harga dengan harga setelah didiskon
+    harga = 200000; // menghitung harga setelah didiskon
+    priceLabel.textContent = `Rp. ${harga.toLocaleString()}`; // memperbarui nilai label harga dengan harga setelah didiskon
   } else {
     alert(`Sorry, coupon code ${couponCode} is not valid.`);
   }
@@ -34,12 +35,9 @@ paymentButton.addEventListener('click', function() {
       alert(`Pembayaran berhasil menggunakan ${paymentMethod} dengan nomor akun ${ewalletAccountNumber}`);
       // mengosongkan nomor akun e-wallet
       document.getElementById('ewallet-account-number').value = '';
-      window.location.href = "PaymentNotifOffline.html";
+      window.location.href = "PaymentNotifOffline.html?bayar=" + harga;
     } else {
       alert('Nomor akun e-wallet harus diisi');
     }
-  } else {
-    alert(`Anda memilih ${paymentMethod}`);
-    window.location.href = "PaymentNotifOffline.html";
-  }
+  } 
 });
